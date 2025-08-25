b8c28c74c788727a4a00cfca81bf66575ae5da28
KJPark23 <147719027+KJPark23@users.noreply.github.com>
2025-08-25 17:53:06 +0200

    Update install-docker.sh

--- ubuntu/install-docker.sh
+++ ubuntu/install-docker.sh
@@ -2,14 +2,29 @@
 set -e
 
 echo "### Installing Docker..."
-curl -fsSL https://get.docker.com | bash
+echo "### Running 5echo.io - Ubuntu Docker installation"
+echo "Removing old docker files"
+sudo for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-echo "### Adding current user to docker group..."
-sudo usermod -aG docker $USER
+echo "Installing apt repository"
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-echo "### Enabling Docker service..."
-sudo systemctl enable docker
-sudo systemctl start docker
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
-echo "### Docker installation complete!"
-docker --version
+echo "Repository added"
+echo "Install the latest Docker packages"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Packages installed"
+echo "Start docker and run system test"
+sudo service docker start && docker --version && sudo docker run hello-world
